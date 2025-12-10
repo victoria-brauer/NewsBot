e47f2e0fb373261425ea94ff0cf77efe095b5ac6
Victoria Brauer <torichip4@gmail.com>
2025-12-10 19:46:48 +0400

    Set up project dependencies

---
+++ app/config.py
@@ -0,0 +1,25 @@
+"""Конфигурация проекта"""
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+    )
+    debug: bool = False
+
+    redis_url: str = 'redis://localhost:6379/0'
+
+    telegram_api_id: int = 0
+    telegram_api_hash: str = ""
+    telegram_bot_token: str = ""
+    telegram_channel_id: str = ""
+
+    news_keywords: str = 'python,fastapi,django,ai,aiogram,нейросети'
+
+    @property
+    def keywords_list(self) -> list(str):
+        raw_value = self.news_keywords
+        parts = [part.strip() for part in raw_value.split(',') if part.strip()]
+        return parts
