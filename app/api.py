73d55ed5bad505ef7ff5f73cd4d0479bdecac4e7
Victoria Brauer <torichip4@gmail.com>
2025-12-23 23:49:28 +0400

    Implemented routes for FastAPI

--- app/api.py
+++ app/api.py
@@ -1,1 +1,36 @@
 """Маршруты для FastAPI"""
+from fastapi import APIRouter, HTTPException, status
+from app.schemas import NewsItem, PublishedNews, Keywords
+
+
+api_router = APIRouter()
+
+
+@api_router.get("/health")
+async def health():
+    return {"status": "ok"}
+
+
+@api_router.get("/news", response_model=list[NewsItem])
+async def news_list() -> list[NewsItem]:
+    fake_news = [
+        NewsItem(
+            id="test1",
+            title="FASTAPI 2.0.1 is released",
+            url="https://fastapi.com/",
+            summary="Короткое описание новости o FastAPI",
+            source="FastAPI blog",
+            published_at="2025-01-01T00:00:00Z",
+            keywords=["python", "fastapi"]
+        ),
+        NewsItem(
+            id="test2",
+            title="Python 3.15.1 is released",
+            url="https://python.org/",
+            summary="Короткое описание новости o Python",
+            source="Python blog",
+            published_at="2025-01-02T00:00:00Z",
+            keywords=["python"]
+        )
+    ]
+    return fake_news
